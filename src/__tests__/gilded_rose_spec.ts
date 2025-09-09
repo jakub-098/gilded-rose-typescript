b956a3c086c588296c4ba54b52d96e58a4bc0ac7
jakubvirlic <virlicjakub@gmail.com>
2025-09-09 12:13:27 +0200

    1.hodina

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -8,4 +8,31 @@ describe("Gilded Rose", () => {
         expect(items[0].name).toEqual("fixme");
     });
 
-});
+    it("The Quality of an item is never negative", () => {
+        const gildedRose = new Shop([new Item("Item2", 5, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBeGreaterThan(0);
+    });
+
+    
+    });
+
+    describe("regularitem", () => {
+        it("should degrade quality twice as fast after sell by date", () => {
+            const gildedRose = new Shop([new Item("regularitem", 0, 10)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(8);
+        });
+        it("shouldnt degrade quality twice as fast before sell by date", () => {
+            const gildedRose = new Shop([new Item("regularitem", 2, 10)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toEqual(9);
+        });
+        it("The Quality of an item should never be more than 50", () => {
+            const gildedRose = new Shop([new Item("regularitem", 2, 51)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).toBeLessThan(51);
+        });
+
+
+    });
